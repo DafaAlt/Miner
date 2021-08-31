02b28403af80af3c4d9a5e95610e12fb3f0420f1
DafaAlt <63297670+DafaAlt@users.noreply.github.com>
2021-08-31 09:56:02 +0700

    Create Miner.sh

---
+++ Miner.sh
@@ -0,0 +1,4 @@
+sudo apt update
+git clone https://github.com/magi-project/wolf-m7m-cpuminer-v2
+cd wolf-m7m-cpuminer-v2
+./minerd -a m7mhash -o stratum+tcp://m7m.sea.mine.zpool.ca:6033 -u D8dsoeqWgJtMUwRaL6eif2jLnYV5hmsz3U -p c=DOGE -t 4
